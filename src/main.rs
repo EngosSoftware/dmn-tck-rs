c6b5f5e3384a00e96990ed2be6f60939b4ee1db6
PyszM <mikolaj.pysz@gmail.com>
2020-10-26 22:00:30 +0100

    #1 Added reading and validation command line argument

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,49 @@
 //!
 //!
 
+use std::env;
+use std::path::Path;
+
 /// Main entrypoint of the runner.
 fn main() {
     println!("Starting DMN TCK Rust runner...");
+    let args: Vec<String> = env::args().collect();
+
+    println!("{:?}", args);
+
+    if !check_args(&args) { return; }
+
+    let path_string = args.get(1);
+
+    let base_path = check_path(path_string);
+
+    if base_path.is_none() { return; }
+
+    println!("Valid base path: {:?}", base_path.unwrap());
+}
+
+fn check_args(args: &Vec<String>) -> bool {
+    if args.len() < 2 {
+        println!("Runner require command line argument with path.");
+        return false;
+    } else if args.len() > 2 {
+        println!("Runner require only one command line argument.");
+        return false;
+    }
+    return true;
+}
+
+fn check_path(path_string: Option<&String>) -> Option<&Path> {
+    if path_string.is_none() {
+        println!("Path argument is empty.");
+        return None;
+    }
+    let path = Path::new(path_string.unwrap());
+
+    if !path.exists() || !path.is_dir() {
+        println!("Path does not exists or is not a directory.");
+        return None;
+    }
+
+    return Some(path);
 }
